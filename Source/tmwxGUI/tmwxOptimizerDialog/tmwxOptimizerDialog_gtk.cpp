d8fc3888b98ba897ce0e94e59a5f47f9fb78c19c
Benjamin Reed <github@raccoonfink.com>
2026-02-28 15:11:07 -0600

    rewrite GTK optimizer dialog for wx3 compatibility

--- Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_gtk.cpp
+++ Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_gtk.cpp
@@ -2,64 +2,34 @@
 File:         tmwxOptimizerDialog_gtk.cpp
 Project:      TreeMaker 5.x
 Purpose:      Source file for class tmwxOptimizerDialog, specialization for GTK
-Author:       Carlos A. Furuti (adapting wxGTK dialog to TM5's model
-                for event handling during long computation)
-Modified by:  
+Author:       Carlos A. Furuti (original wx2 GTK version)
+Modified by:
 Created:      2004-04-22
-Copyright:    ©1998 Robert Roebling (original wxGTK code, released
-                under the wxWidgets license)
-Copyright:    ©2004 Carlos A. Furuti. All Rights Reserved.
+Copyright:    (c)2004 Carlos A. Furuti. All Rights Reserved.
 *******************************************************************************/
 
 #ifdef __WXGTK__
 
-#include <gtk/gtk.h>
-
-#include "tmwxStr.h"
 #include "tmwxOptimizerDialog.h"
 
-extern int g_openDialogs;
-
-void tmwxOptimizerDialog::DoStartModal() {
-  /* CAF - essentially lifted from wxGTK 2.5.1's wxDialog::ShowModal, up to
-     grabbing the focus. */
-    if (IsModal()) {
-       wxFAIL_MSG( wxT("wxDialog:ShowModal called twice") );
-       mStatus = GetReturnCode();
-       return;
-    }
-
-    // use the apps top level window as parent if none given unless explicitly
-    // forbidden
-    if (! GetParent() && !(GetWindowStyleFlag() & wxDIALOG_NO_PARENT)) {
-        wxWindow *parent = wxTheApp->GetTopWindow();
-        if (parent && parent != this &&
-            parent -> IsBeingDeleted() &&
-            ! (parent->GetExtraStyle() & wxWS_EX_TRANSIENT)) {
-            m_parent = parent;
-            gtk_window_set_transient_for (GTK_WINDOW(m_widget),
-            GTK_WINDOW(parent->m_widget) );
-        }
-    }
-
-    wxBeginBusyCursor ();
-    Show (true);
-    SetFocus();
-    m_modalShowing = true;
-    g_openDialogs++;
-    gtk_grab_add (m_widget);
+void tmwxOptimizerDialog::DoStartModal()
+{
+  ::wxBeginBusyCursor();
+  Show(true);
+  SetFocus();
 }
 
-void tmwxOptimizerDialog::DoEventLoopOnce() {
-  while (gtk_events_pending ())
-    gtk_main_iteration ();
+
+void tmwxOptimizerDialog::DoEventLoopOnce()
+{
+  wxYield();
 }
 
-void tmwxOptimizerDialog::DoFinishModal() {
-    gtk_grab_remove (m_widget);
-    g_openDialogs--;
-    wxEndBusyCursor ();
+
+void tmwxOptimizerDialog::DoFinishModal()
+{
+  Show(false);
+  ::wxEndBusyCursor();
 }
 
-#endif  // __WXGTK__
-
+#endif // __WXGTK__
