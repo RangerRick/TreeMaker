c3991cc2fe447af82bd19e17e7d1bc3e8bb9ed42
Benjamin Reed <github@raccoonfink.com>
2026-02-28 14:50:47 -0600

    replace C-style casts with static_cast in tmPath destructor

--- Source/tmModel/tmTreeClasses/tmPath.cpp
+++ Source/tmModel/tmTreeClasses/tmPath.cpp
@@ -171,8 +171,8 @@ were attached to it. We rely on this behavior in many places.
 *****/
 tmPath::~tmPath()
 {
-  if (mFwdPoly != nullptr) delete (tmPoly*) mFwdPoly;
-  if (mBkdPoly != nullptr) delete (tmPoly*) mBkdPoly;
+  if (mFwdPoly != nullptr) delete static_cast<tmPoly*>(mFwdPoly);
+  if (mBkdPoly != nullptr) delete static_cast<tmPoly*>(mBkdPoly);
 }
 
 
